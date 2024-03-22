84463137ffbb6704bc1130d3314a8fbc3e720d04
elruco <ing.garcia1995@gmail.com>
2024-03-22 12:15:36 -0600

    This is a rock,paper,scissors game
    everytime you push the button it will start a 5 round game.
    The game memory store every round result. A reset function is required

---
+++ game.js
@@ -0,0 +1,74 @@
+function getComputerChoice () { // this function give us a random choice
+    const items = ['rock', 'paper', 'scissors'];
+    randomChoice = items[Math.floor(Math.random()*items.length)];
+    return randomChoice;
+}
+
+function playRound (playerSelection,computerSelection) {  // this is the game
+    if (playerSelection !== 'paper' && playerSelection !== 'rock' && playerSelection !=='scissors') {
+        window.alert('input no valid');
+    } else {
+        switch(true) {
+            case playerSelection == 'rock' && computerSelection == 'scissors':
+                return 'You win! ' + playerSelection +' beats '+ computerSelection;
+                break;
+            case playerSelection == 'paper' && computerSelection == 'rock':
+                return 'You win! ' + playerSelection +' beats '+ computerSelection;
+                break;
+            case playerSelection == 'scissors' && computerSelection == 'paper':
+                return 'You win! ' + playerSelection +' beats '+ computerSelection;
+                break;
+            case playerSelection == 'scissors' && computerSelection == 'rock':
+                return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            case playerSelection == 'rock' && computerSelection == 'paper':
+                return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            case playerSelection == 'paper' && computerSelection == 'scissors':
+                return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+                break;
+            default:
+                return "it's a tie! " + playerSelection + ' VS ' + computerSelection;
+        }
+    }
+}
+
+let gameMemo=[];
+function countWinLoss(){ // this records the final results
+let winCount=0;
+let lossCount=0;
+let tieCount=0
+    for(let i=0; i < gameMemo.length; i++){
+        if (gameMemo[i].includes('You win! ')){
+            winCount++;
+        }else if (gameMemo[i].includes('You lose! ')){
+            lossCount++;
+        }else{
+            tieCount++;
+        }
+        
+    }
+    return [winCount, lossCount, tieCount]
+}
+
+function winner(){ // this determines if the player is a winner or a loser
+    if (countWinLoss()[0] > countWinLoss()[1]) {
+        return  'Winner! ' + countWinLoss()[0] + ' wins vs ' + countWinLoss()[1] + ' losses.'
+    } else {
+        return 'Looser!' + countWinLoss()[0] + ' wins vs ' + countWinLoss()[1] + ' losses.'
+    }
+}
+
+function playGame(){ // this allows us to play 5 rounds and record each round results
+    for(let i=0; i<5; i++){
+            let computerSelection = getComputerChoice();
+            let playerSelection = prompt('Select between paper, scissors and rock');
+            gameMemo.push((playRound(playerSelection,computerSelection)));
+            console.log(gameMemo);
+    }
+    console.log(winner());
+   
+}
+const promptButton = document.getElementById('prompt');
+promptButton.addEventListener('click',playGame)
+;
